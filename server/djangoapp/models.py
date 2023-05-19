bce11fc3f784a0352620339290e42a656d6173f7
Navneeth <navneeth0112@gmail.com>
2023-05-19 05:43:53 -0400

    Car

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+    desc = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -20,7 +25,15 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+class CarModel(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+    id = models.CharField(max_length=2)
+    type = models.CharField(max_length=10)
+    year = models.DateField(null=True)
+    carmakes = models.ForeignKey(CarMake,on_delete= models.CASCADE,default='default_car_make')
 
+    def __str__(self):
+        return self.name
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
